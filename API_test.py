42f987ddf6c3e3b431d8fb066e5be56f299408b4
Paul Kenntner <paul7pk@gmail.com>
2025-02-06 11:55:27 +0100

    Cleaned Up Ranking

--- API_test.py
+++
@@ -1,8 +0,0 @@
-from app.api.routes import get_rank
-
-async def test_get_rank():
-    restaurant_slug = "loco-chicken-i-frechen"
-    result = await get_rank(restaurant_slug)
-    print(result)
-
-test_get_rank()
